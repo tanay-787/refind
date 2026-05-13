bc580334182d89de474a6c85101b062953bb4bf2
tanay-787 <tanayyyyy7@gmail.com>
2026-05-13 19:37:53 +0530

    added gradle path

--- android-build.sh
+++ android-build.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 export JAVA_HOME="${JAVA_HOME:-/home/tanay/opt/jdks/jdk17}"
+export GRADLE_USER_HOME="${GRADLE_USER_HOME:-/home/tanay/.gradle}"
 export PATH="$JAVA_HOME/bin:$PATH"
 
 cd /devspace/projects/ss-search
